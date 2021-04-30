bb795ad2271514cc9149c5c1a318d0c9aec749e2
Victor Mesquita <mesquitadev@gmail.com>
2021-04-30 09:56:22 -0300

    M - Changed HTTP Verb on PeopleController

--- src/main/java/com/dio/personapi/controller/PersonController.java
+++ src/main/java/com/dio/personapi/controller/PersonController.java
@@ -1,11 +1,13 @@
 package com.dio.personapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
+    @GetMapping
     public String index() {
         return "Index OK!";
     }
